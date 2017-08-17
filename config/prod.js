63a71bd1bcc126cc06417593a5caf496d4bd171b
PEH2 <howley.phil@gmail.com>
2017-08-16 22:50:48 -0700

    add proxy info for redirect

--- config/prod.js
+++ config/prod.js
@@ -3,5 +3,6 @@ module.exports = {
   googleClientID: process.env.GOOGLE_CLIENT_ID,
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
   mongoURI: process.env.MONGO_URI,
-  randomCookieKey: process.env.RANDOM_COOKIE_KEY
+  randomCookieKey: process.env.RANDOM_COOKIE_KEY,
+  googleRedirectURI: 'http://localhost:5000/auth/google'
 };
